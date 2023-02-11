5854325ad3cf8bea14954baa20be71f4a36ef4ac
charles-hsuz <charles_hsu@qq.com>
2023-02-11 10:41:52 +0800

    1.0.2 第一个正式版

--- index.js
+++ index.js
@@ -1,46 +1,25 @@
 let config = hexo.config.copyMarkdown
 let pureMarkdown = config.pureMarkdown || false
-let keyboard = config.keyboard || false
 let field = config.field || 'post'
-let copyright_enable = config.copyright.enable || true
-let copyright_container = config.copyright.container || '#article-container'
-let Reprint_enable = config.Reprint.enable || true
-let Reprint_container = config.Reprint.container || '.post-copyright'
-
+let copyright_enable = config.copyright ?? true
+let Reprint_enable = config.Reprint ?? true
 
 const css = hexo.extend.helper.get('css').bind(hexo);
-const fs = require('fs');
-const path = require('path');
-const jsPath = path.join(__dirname, 'lib', 'copyMarkdown.js');
 const js = hexo.extend.helper.get('js').bind(hexo);
 
-hexo.extend.filter.register('after_generate', function(){
-  if(hexo.config.copyMarkdown.enable){
-    fs.readFile(jsPath, 'utf-8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      const content = js("https://unpkg.com/turndown/dist/turndown.js") + `<script data-pjax>${data}</script>`;
-      hexo.extend.injector.register('body_end', content, "post");
-    });
-  }
-  hexo.extend.injector.register('head_end', () => {
-    return `<style type="text/css">#reprintedButton {
-      display: flex;
-      height: 0;
-      flex-direction: row-reverse;
+hexo.extend.filter.register('after_generate', function () {
+  if (hexo.config.copyMarkdown.enable) {
+    var content = `<script data-pjax>const copyright_enable = ${copyright_enable} </script>` + js("https://unpkg.com/turndown/dist/turndown.js");
+    if (!pureMarkdown) content += js('https://cdn.jsdelivr.net/npm/hexo-butterfly-copymarkdown@1.0.2/lib/copyMarkdown.js');
+    else content += js('https://cdn.jsdelivr.net/npm/hexo-butterfly-copymarkdown@1.0.2/lib/pureMarkdown.js');
+    if (Reprint_enable) {
+      content += js('https://cdn.jsdelivr.net/npm/hexo-butterfly-copymarkdown@1.0.2/lib/reprint.js');
+      hexo.extend.injector.register('head_end', () => {
+        return css('https://cdn.jsdelivr.net/npm/hexo-butterfly-copymarkdown@1.0.2/lib/reprint.css');
+      }, 'post');
     }
-    
-    #reprintedButton i{
-      position: relative;
-      font-size: 34px;
-      top: -25px;
-      right: -10px;
-      color: #e5e5e5;
-      cursor: pointer;
-    }</style>`;
-  }, 'post');  
+    hexo.extend.injector.register('body_end', content, field);
+  }
 });
 
 
